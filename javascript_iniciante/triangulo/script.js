522c4c6f38e8a120582e002ff792b17a1bc65a0f
Cristiano Lima <cristiano.lima.ribeiro@gmail.com>
2019-03-19 11:35:11 -0300

    usando for para desenhar traingulo com *

---
+++ javascript_iniciante/triangulo/script.js
@@ -0,0 +1,14 @@
+for (var i = 0; i < 10; i++) {
+    linha = '';
+    for(var j = 0; j < i; j++){
+        linha += '*';
+    }
+    console.log(linha);
+}
+for (var i = 10; i > 0; i--) {
+    linha = '';
+    for(var j = 0; j < i; j++){
+        linha += '*';
+    }
+    console.log(linha);
+}
